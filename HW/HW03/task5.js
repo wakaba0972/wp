a22ee318b147eb728fc0761d31e159bb9db301e0
wakaba0972 <benny.wu2199@gmail.com>
2024-03-15 12:47:29 +0800

    fix task5

--- HW/HW03/task5.js
+++ HW/HW03/task5.js
@@ -4,12 +4,12 @@
 function vectorAdd(a, b){
     c = []
 
-    for(let i=0; i<max(a.length, b.length); i++){
+    for(let i=0; i<Math.max(a.length, b.length); i++){
         if(!a[i]) a[i] = 0;
         if(!b[i]) b[i] = 0;
 
         c[i] = a[i] + b[i];
     }
 
-    return c[i];
+    return c;
 }
