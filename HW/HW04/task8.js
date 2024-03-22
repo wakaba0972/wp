676b75dd6eab0f9c1de9a06784e4528b64f8a612
wakaba0972 <benny.wu2199@gmail.com>
2024-03-22 11:24:06 +0800

    fix

--- HW/HW04/task8.js
+++ HW/HW04/task8.js
@@ -3,15 +3,15 @@
 const h = 0.0001;
 
 function f(p){
-    return 3 * p[0] ** 2 + p[1];
+    return 3 * p[0] ** 2 + 5 * p[1] * p[2];
 }
 
 //p[0]為x p[1]為y
 //若k=0 則是對x微分 以此類推
 function df(f, p, k){
     let p1 = Array.from(p);
-
-    return (f(p1[k] + h) - f(p)) / h
+    p1[k] += h;
+    return (f(p1) - f(p)) / h
 }
 
 //對每個維度微分, 放進ps裡回傳
@@ -24,3 +24,4 @@ function grad(f, p){
     return ps;
 }
 
+console.log(grad(f, [4,5,6]));
